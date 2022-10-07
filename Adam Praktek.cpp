2c428e035631eac3f5b7d2ae4b9aa6d13fd2dddf
AdamSandyAa <114906190+AdamSandyAa@users.noreply.github.com>
2022-10-07 16:02:39 +0700

    Mengumpulkan tugas praktikum 1
    ini adalah tugas cpp saya kangg :)

---
+++ Adam Praktek.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iostream>
+
+using namespace std;
+
+int main (){
+int umur = 18;
+string nama = "Adam Sandy Azhari";
+long long npm = 2210631170108;
+string Goals = "Berguna";
+string Hobi = "Menggambar";
+string Kelas = "1C informatika";
+cout << "NPM : " << npm << endl;
+cout << "nama lengkap : " << nama << endl;
+cout << "umur : " << umur << endl;
+cout << "hobi : " << Hobi << endl;
+cout << "cita-cita : " << Goals << endl;
+}
---
+++ adam tugas 2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+// tugas 2
+//Menukarkan dua variable dengan bantuan Variable Sementara
+int a,b,c;
+a = 10;//Variable awal
+b = 5;//Variable awal
+c = a;// Variable c dituker dengan Variable a yang bernilai 5
+a = b;//Variable b ditukar dengan Variable b yang bernilai 10
+b = c;// Variable b ditukar dengan Variable c yang bernilai 5
+
+cout<< "Menekarkan 2 Variable dengan bantuan Variable Sementara\n";
+    cout<<"A:"<< a <<endl<<"B:"<<b<<endl;
+
+
+
+}
